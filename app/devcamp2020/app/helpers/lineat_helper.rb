6b54241eb37243a516f730bdb2a623ddff6df11a
Ikuma Hayashi <ikuma.hayashi.tech@gmail.com>
2020-01-01 23:34:51 +0900

    add:コントローラー追加

---
+++ app/devcamp2020/app/helpers/lineat_helper.rb
@@ -0,0 +1,2 @@
+module LineatHelper
+end
---
+++ app/devcamp2020/test/controllers/lineat_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LineatControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
